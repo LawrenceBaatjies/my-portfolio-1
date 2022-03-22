e1acd32573bb471f4377cd1817dcc2b750ec0939
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-03-22 23:01:32 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
-  if (event.currentTarget.className.includes('carousel-control')) { return }
+  if (event.currentTarget.className.includes("carousel-control")) { return }
 
   event.preventDefault();
 
   const offsetToScroll = possibleMobile() ? 0 : 20;
 
   $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top - offsetToScroll
+      scrollTop: $($.attr(this, 'href')).offset().top - offsetToScroll
   }, 'slow');
 
   $('label.hamburguer-menu input').prop('checked', false);
@@ -14,14 +14,14 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 
 const showNavbar = function() {
   const headerHeight = $('header').height() - 70;
-  if ($(window).scrollTop() > headerHeight) {
-    $('nav').parent().removeClass('hidden-content');
-    $('label.hamburger-menu').removeClass('hidden-content');
-    $('#header-text').hide()
+  if($(window).scrollTop() > headerHeight) {
+      $('nav').parent().removeClass('hidden-content');
+      $('label.hamburguer-menu').removeClass('hidden-content');
+      $('#header-text').hide()
   } else if($(window).scrollTop() < headerHeight) {
-    $('nav').parent().addClass('hidden-content');
-    $('label.hamburger-menu').addClass('hidden-content');
-    $('#header-text').show()
+      $('nav').parent().addClass('hidden-content');
+      $('label.hamburguer-menu').addClass('hidden-content');
+      $('#header-text').show()
   }
 };
 
